cad670c885d0dc5a18aba31ebd6fe1673e0fcc29
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2015-09-06 16:29:21 +0530

    Instantiate MongoEngine in application context
    (cherry picked from commit 00a1fe7493d0b8b9421b7ef7af4493fb98b8abe4)

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,5 +1,11 @@
 """ Application module """
 
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {'DB': 'flask_mongoengine'}
+app.config['SECRET_KEY'] = 'SEFO36867TPL9Yl77c5iYTC3IVjGTWtp'
+
+db = MongoEngine(app)
