a1a2ffaa16d999f8209672b628648663d63b5b78
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2015-09-10 00:10:51 +0530

    Comment form view and template

--- src/views/detail.py
+++ src/views/detail.py
@@ -1,17 +1,51 @@
 """ Detail view """
 
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from flask.views import MethodView
+from flask.ext.mongoengine.wtf import model_form
 
-from ..models import Post
+from ..models import Post, Comment
 
 
 class DetailView(MethodView):
     """ Detail view definition """
 
-    def get(self, slug):
-        """ Render detail template """
+    form = model_form(Comment, exclude=['created_at'])
+
+    def get_context(self, slug):
+        """ Create context for template """
 
         post = Post.objects.get_or_404(slug=slug)
+        form = self.form(request.form)
 
-        return render_template('posts/detail.html', post=post)
+        context = {
+            'post': post,
+            'form': form
+        }
+
+        return context
+
+    def get(self, slug):  # pylint: disable=I0011,R0201
+        """ Render detail template """
+
+        context = self.get_context(slug)
+
+        return render_template('posts/detail.html', **context)
+
+    def post(self, slug):
+        """ Handle comment form post """
+
+        context = self.get_context(slug)
+        form = context.get('form')
+
+        if form.validate():
+            comment = Comment()
+            form.populate_obj(comment)
+
+            post = context.get('post')
+            post.comments.append(comment)
+            post.save()
+
+            return redirect(url_for('posts.detail', slug=slug))
+
+        return render_template('posts/detail.html', **context)
